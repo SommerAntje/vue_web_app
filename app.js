0a6322353924d8fb428b6a4e397484d3d76eccda
Antje Sommer <antje.som@gmail.com>
2019-09-25 10:53:20 +0200

    add error handling and loading condition

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#app',
     data() {
         return {
-            info: null
+            info: null,
+            loading: true,
+            errored: false
         }
     },
     filters: {
@@ -13,6 +15,13 @@ new Vue({
     mounted() {
         axios
             .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => (this.info = response.data.bpi))
+            .then(response => {
+                this.info = response.data.bpi
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => this.loading = false)
     }
 })
